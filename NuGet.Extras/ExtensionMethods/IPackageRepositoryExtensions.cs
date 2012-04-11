36acb0b07c2409694268e7b136909ab0e83d08ac
Ben Phegan <benphegan@gmail.com>
2012-04-11 10:27:43 +1000

    Fix for FindLatestPackage, adding a .gitignore.

--- NuGet.Extras/ExtensionMethods/IPackageRepositoryExtensions.cs
+++ NuGet.Extras/ExtensionMethods/IPackageRepositoryExtensions.cs
@@ -7,16 +7,30 @@ namespace NuGet.Extras.ExtensionMethods
 {
     public static class IPackageRepositoryExtensions
     {
+        /// <summary>
+        /// Finds the latest package in a repository by Package Id
+        /// </summary>
+        /// <param name="repository"></param>
+        /// <param name="packageId"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public static IPackage FindLatestPackage(this IPackageRepository repository, string packageId)
         {
             if (packageId == null)
             {
                 throw new ArgumentNullException("packageId");
             }
-			
-			return repository.GetPackages().Where((p) => p.IsLatestVersion && p.Id.Equals(packageId,StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
+            return repository.GetPackages().Where(p => p.Id.Equals(packageId, StringComparison.OrdinalIgnoreCase)).Where(p => p.IsLatestVersion).FirstOrDefault();			
         }
 
+        /// <summary>
+        /// Finds the latest package in a repository constrained by an Id and an IVersionSpec
+        /// </summary>
+        /// <param name="repository"></param>
+        /// <param name="packageId"></param>
+        /// <param name="versionSpec"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public static IPackage FindLatestPackage(this IPackageRepository repository, string packageId, IVersionSpec versionSpec)
         {
             if (packageId == null)
